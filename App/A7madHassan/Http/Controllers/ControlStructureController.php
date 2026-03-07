f03108a28e0395298e8727618338e44ae3d5044a
Maged Yaseen <magedyaseen@hotmail.com>
2026-03-07 22:36:22 +0200

    Fix issues by one of the team

--- App/A7madHassan/Http/Controllers/ControlStructureController.php
+++ App/A7madHassan/Http/Controllers/ControlStructureController.php
@@ -30,39 +30,44 @@ class ControlStructureController
         }
     }
 
-    public static function temp(float $temp): string {
+    public static function temp(float $temp): string
+    {
         return "The temp is: $temp";
     }
-public static function weatherCondition($temp){
-if($temp >=35){
-    echo"too hot";
-}elseif($temp>=25){ echo"Normal<br>";}
-elseif($temp>=15){ echo"Coll<br>";}
-elseif($temp>=5){ echo"Cold<br>";}
-else {echo "Frozen<br>";}
-
-
-}
-
-
-public static function weekDaysMessage($dayName) {
-    $day = ucfirst(strtolower($dayName));
-
-    switch ($day) {
-        case 'Friday':
-        case 'Saturday':
-            return "Happy weekend";
-        
-        case 'Sunday':
-        case 'Monday':
-        case 'Tuesday':
-        case 'Wednesday':
-        case 'Thursday':
-            return "Enjoy your work ";
-
-        default:
-            return "Invalid day name! Please enter a valid week day.";
+    public static function weatherCondition($temp)
+    {
+        if ($temp >= 35) {
+            echo "too hot";
+        } elseif ($temp >= 25) {
+            echo "Normal<br>";
+        } elseif ($temp >= 15) {
+            echo "Coll<br>";
+        } elseif ($temp >= 5) {
+            echo "Cold<br>";
+        } else {
+            echo "Frozen<br>";
+        }
     }
-}
 
+
+    public static function weekDaysMessage($dayName)
+    {
+        $day = ucfirst(strtolower($dayName));
+
+        switch ($day) {
+            case 'Friday':
+            case 'Saturday':
+                return "Happy weekend";
+
+            case 'Sunday':
+            case 'Monday':
+            case 'Tuesday':
+            case 'Wednesday':
+            case 'Thursday':
+                return "Enjoy your work ";
+
+            default:
+                return "Invalid day name! Please enter a valid week day.";
+        }
+    }
 }
