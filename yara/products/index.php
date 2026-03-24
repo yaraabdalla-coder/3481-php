72fadd8e69e1049f121ea2eb4ae898dcfa09e838
yara abdalla <yara abdalla@gmail com>
2026-03-24 13:42:18 -0700

    api

---
+++ yara/products/index.php
@@ -0,0 +1,216 @@
+<?php
+$products = [
+    [
+        "id" => 1,
+        "firstName" => "Emily",
+        "lastName" => "Johnson",
+        "age" => 29,
+        "gender" => "female"
+    ],
+    [
+        "id" => 2,
+        "firstName" => "Michael",
+        "lastName" => "Williams",
+        "age" => 36,
+        "gender" => "male"
+    ],
+    [
+        "id" => 3,
+        "firstName" => "Sophia",
+        "lastName" => "Brown",
+        "age" => 43,
+        "gender" => "female"
+    ],
+    [
+        "id" => 4,
+        "firstName" => "James",
+        "lastName" => "Davis",
+        "age" => 46,
+        "gender" => "male"
+    ],
+    [
+        "id" => 5,
+        "firstName" => "Emma",
+        "lastName" => "Miller",
+        "age" => 31,
+        "gender" => "female"
+    ],
+    [
+        "id" => 6,
+        "firstName" => "Olivia",
+        "lastName" => "Wilson",
+        "age" => 23,
+        "gender" => "female"
+    ],
+    [
+        "id" => 7,
+        "firstName" => "Alexander",
+        "lastName" => "Jones",
+        "age" => 39,
+        "gender" => "male"
+    ],
+    [
+        "id" => 8,
+        "firstName" => "Ava",
+        "lastName" => "Taylor",
+        "age" => 28,
+        "gender" => "female"
+    ],
+    [
+        "id" => 9,
+        "firstName" => "Ethan",
+        "lastName" => "Martinez",
+        "age" => 34,
+        "gender" => "male"
+    ],
+    [
+        "id" => 10,
+        "firstName" => "Isabella",
+        "lastName" => "Anderson",
+        "age" => 32,
+        "gender" => "female"
+    ],
+    [
+        "id" => 11,
+        "firstName" => "Liam",
+        "lastName" => "Garcia",
+        "age" => 30,
+        "gender" => "male"
+    ],
+    [
+        "id" => 12,
+        "firstName" => "Mia",
+        "lastName" => "Rodriguez",
+        "age" => 25,
+        "gender" => "female"
+    ],
+    [
+        "id" => 13,
+        "firstName" => "Noah",
+        "lastName" => "Hernandez",
+        "age" => 41,
+        "gender" => "male"
+    ],
+    [
+        "id" => 14,
+        "firstName" => "Charlotte",
+        "lastName" => "Lopez",
+        "age" => 37,
+        "gender" => "female"
+    ],
+    [
+        "id" => 15,
+        "firstName" => "William",
+        "lastName" => "Gonzalez",
+        "age" => 33,
+        "gender" => "male"
+    ],
+    [
+        "id" => 16,
+        "firstName" => "Avery",
+        "lastName" => "Perez",
+        "age" => 26,
+        "gender" => "female"
+    ],
+    [
+        "id" => 17,
+        "firstName" => "Evelyn",
+        "lastName" => "Sanchez",
+        "age" => 38,
+        "gender" => "female"
+    ],
+    [
+        "id" => 18,
+        "firstName" => "Logan",
+        "lastName" => "Torres",
+        "age" => 32,
+        "gender" => "male"
+    ],
+    [
+        "id" => 19,
+        "firstName" => "Abigail",
+        "lastName" => "Rivera",
+        "age" => 29,
+        "gender" => "female"
+    ],
+    [
+        "id" => 20,
+        "firstName" => "Jackson",
+        "lastName" => "Evans",
+        "age" => 35,
+        "gender" => "male"
+    ],
+    [
+        "id" => 21,
+        "firstName" => "Madison",
+        "lastName" => "Collins",
+        "age" => 27,
+        "gender" => "female"
+    ],
+    [
+        "id" => 22,
+        "firstName" => "Elijah",
+        "lastName" => "Stewart",
+        "age" => 34,
+        "gender" => "male"
+    ],
+    [
+        "id" => 23,
+        "firstName" => "Chloe",
+        "lastName" => "Morales",
+        "age" => 40,
+        "gender" => "female"
+    ],
+    [
+        "id" => 24,
+        "firstName" => "Mateo",
+        "lastName" => "Nguyen",
+        "age" => 31,
+        "gender" => "male"
+    ],
+    [
+        "id" => 25,
+        "firstName" => "Harper",
+        "lastName" => "Kelly",
+        "age" => 28,
+        "gender" => "female"
+    ],
+    [
+        "id" => 26,
+        "firstName" => "Evelyn",
+        "lastName" => "Gonzalez",
+        "age" => 36,
+        "gender" => "female"
+    ],
+    [
+        "id" => 27,
+        "firstName" => "Daniel",
+        "lastName" => "Cook",
+        "age" => 42,
+        "gender" => "male"
+    ],
+    [
+        "id" => 28,
+        "firstName" => "Lily",
+        "lastName" => "Lee",
+        "age" => 30,
+        "gender" => "female"
+    ],
+    [
+        "id" => 29,
+        "firstName" => "Henry",
+        "lastName" => "Hill",
+        "age" => 39,
+        "gender" => "male"
+    ],
+    [
+        "id" => 30,
+        "firstName" => "Addison",
+        "lastName" => "Wright",
+        "age" => 33,
+        "gender" => "female"
+    ]
+];
+
+$products_json = json_encode($products);
+echo $products_json;
